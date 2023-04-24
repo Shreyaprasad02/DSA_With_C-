bf83d6a3ac5b9218e8a7459efdfe72c60b362db3
Shreya <shreyaprasadaug@gmail.com>
2023-04-24 16:18:01 +0530

    Initial commit

--- Introduction to C++/Conditionals and Loops/number_pattern_2.cpp
+++ Introduction to C++/Conditionals and Loops/number_pattern_2.cpp
@@ -8,3 +8,29 @@ Pattern for N = 4
 The dots represent spaces.
 */
 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i <= n){
+        int spaces = 1;
+        while (spaces <= n - i)
+        {
+            cout << " ";
+            spaces = spaces + 1;
+        }
+        int col = 1;
+        int value = i;
+        while(col <= i){
+            cout << value;
+            value = value + 1;
+            col = col + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }    
+}
