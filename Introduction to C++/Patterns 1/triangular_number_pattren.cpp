b9480268205532f96fc9c06c795ea1db6c619099
Shreya <shreyaprasadaug@gmail.com>
2023-04-25 21:33:37 +0530

    Initial commit

---
+++ Introduction to C++/Patterns 1/triangular_number_pattren.cpp
@@ -0,0 +1,30 @@
+/*
+Code : Triangle Number Pattern
+
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+22
+333
+4444
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int currRow = 1;
+    while(currRow<=n){
+        int currCol = 1;
+        while (currCol <= currRow)
+        {
+            cout << currRow;
+            currCol = currCol + 1;
+        }
+        cout << endl;
+        currRow = currRow + 1;
+    }  
+}
