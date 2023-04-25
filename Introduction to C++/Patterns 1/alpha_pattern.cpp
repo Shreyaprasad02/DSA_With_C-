1fb9e4dcc8085196cff790efe9d0bd412c9d90a4
Shreya <shreyaprasadaug@gmail.com>
2023-04-25 21:44:37 +0530

    Initial commit

---
+++ Introduction to C++/Patterns 1/alpha_pattern.cpp
@@ -0,0 +1,31 @@
+/*
+Code : Alpha Pattern
+
+Print the following pattern for the given N number of rows.
+Pattern for N = 3
+ A
+ BB
+ CCC
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int currRow = 1;
+    while(currRow<=n){
+        int currCol = 1;
+        char ch = 'A' + currRow - 1;
+        while (currCol <= currRow)
+        {
+            cout << ch;
+            currCol = currCol + 1;
+        }
+        cout << endl;
+        currRow = currRow + 1;
+    }  
+}
