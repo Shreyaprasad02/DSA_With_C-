f1c1b818cf9ead2c5e84befb213bb31cf3d44386
Shreya <shreyaprasadaug@gmail.com>
2023-04-26 22:32:52 +0530

    Initial commit

---
+++ Introduction to C++/Patterns 2/star_pattern.cpp
@@ -0,0 +1,37 @@
+/*
+Code : Star Pattern
+Send Feedback
+Print the following pattern
+Pattern for N = 4
+
+. . . *
+. . * * *
+. * * * * *
+* * * * * * *
+
+The dots represent spaces.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int currRow = 1;
+    while(currRow<=n){
+        int spaces = 1;
+        while(spaces <= n - currRow){
+            cout << " ";
+            spaces = spaces + 1;
+        }
+        int currCol = 1;
+        while (currCol <= (2 * currRow) - 1){
+            cout << "*";
+            currCol = currCol + 1;
+        }
+        cout << endl;
+        currRow = currRow + 1;
+    }
+}    
