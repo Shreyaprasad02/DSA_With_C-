a223cfcc969eebe4a3287ad3f2af2e05bd0ba8f5
Shreya <shreyaprasadaug@gmail.com>
2023-04-29 21:09:13 +0530

    Initial commit

---
+++ Introduction to C++/Test 1/star_number_pattern.cpp
@@ -0,0 +1,44 @@
+/*
+Number Star Pattern
+
+Print the following pattern for given number of rows.
+Input format :
+
+Line 1 : N (Total number of rows)
+
+Sample Input :
+   5
+Sample Output :
+1234554321
+1234**4321
+123****321
+12******21
+1********1
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  for(int i =1; i<=n; i++){
+    for(int j=1; j<=n; j++){
+      if(j>(n - i + 1)){
+        cout<< '*';
+      }
+      else{
+        cout<<j;
+      }
+    }
+    for(int j =1; j<=n; j++){
+      if(j < i){
+        cout<<'*';
+      }
+      else{
+        cout<<n-j+1;
+      }
+    }
+    cout<<endl;
+  }
+}
