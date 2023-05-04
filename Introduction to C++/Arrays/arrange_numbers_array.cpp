b525f84c551dba71b3253cae94db16f2b75c62fb
Shreya <shreyaprasadaug@gmail.com>
2023-05-04 23:05:33 +0530

    Initial commit

--- Introduction to C++/Arrays/arrange_numbers_array.cpp
+++ Introduction to C++/Arrays/arrange_numbers_array.cpp
@@ -21,14 +21,18 @@ Time Limit: 1sec
 Sample Input 1 :
 1
 6
+
 Sample Output 1 :
 1 3 5 6 4 2
+
 Explanation of Sample Input 1 :
 Since the value of N is 6, the number will be stored in the array in such a fashion that 1 will appear at 0th index, then 2 at the last index, in a similar fashion 3 is stored at index 1. Hence the array becomes 1 3 5 6 4 2.
+
 Sample Input 2 :
 2
 9
 3
+
 Sample Output 2 :
 1 3 5 7 9 8 6 4 2
 1 3 2
