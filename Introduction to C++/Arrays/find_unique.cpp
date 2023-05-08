880110784fdc638e0314e59f7bd1f571cd3e1068
Shreya <shreyaprasadaug@gmail.com>
2023-05-08 23:03:03 +0530

    Initial commit

--- Introduction to C++/Arrays/find_unique.cpp
+++ Introduction to C++/Arrays/find_unique.cpp
@@ -4,7 +4,8 @@ Find Unique
 You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
 Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
 You need to find and return that number which is unique in the array/list.
- Note:
+
+Note:
 Unique element is always present in the array/list according to the given condition.
 Input format :
 The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
